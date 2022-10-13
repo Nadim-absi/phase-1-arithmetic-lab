61a3bee10806de0720846168af8846c5f770481f
Nadim Absi <absinad1@gmail.com>
2022-10-13 09:13:13 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const num1 =2
+const num2 =31
+const num3 =18
+const num4 =22
+let multiply =num1*num2
+const random = Math.floor(Math.random() * 10) + 1;
+const mod =num4%num3
+const max =Math.max(1,2,20)
 
 // Write your code here
